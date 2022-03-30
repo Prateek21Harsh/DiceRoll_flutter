14045fcb7afcb611ef0ba647ea8d9daa08d54e3c
PrateekHarsh21 <prateek@prachet.com>
2022-03-30 23:22:49 +0530

    temp

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  //const DicePage({Key? key}) : super(key: key);
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDNum = 1;
+  int rightDNum = 2;
+
+  void changeFace(){
+    setState(() {
+      leftDNum = Random().nextInt(6) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeFace();
+              },
+              child: Image(
+                image : AssetImage('images/dice$leftDNum.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDNum = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$rightDNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
